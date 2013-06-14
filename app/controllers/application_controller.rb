12b5f8eac061344f71a2a509fb894f67301b859e
James Haville <james.haville@rulefinancial.com>
2013-06-14 18:54:47 +0100

    added support for older safari and changed message for out of date browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     Browser = Struct.new(:browser, :version)
 
     SupportedBrowsers = [
-      Browser.new('Safari', '6.0.2'),
+      Browser.new('Safari', '5.1.7'),
       Browser.new('Firefox', '19.0.2'),
       Browser.new('Internet Explorer', '9.0'),
       Browser.new('Chrome', '25.0.1364.160')
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
     def check_browser
       user_agent = UserAgent.parse(request.user_agent)
       unless SupportedBrowsers.detect { |browser| user_agent >= browser }
-        render text: 'Your browser is not supported!'
+        render text: 'Your browser is not supported! Please upgrade your browser to view our website!'
       end
     end
 end
