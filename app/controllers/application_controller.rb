6142265f1efe534414a3612bfb681995ec24f1a2
James Haville <james.haville@rulefinancial.com>
2013-07-09 14:44:18 +0100

    only not supported under ie9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-    Browser = Struct.new(:browser, :version)
-
-    SupportedBrowsers = [
-      Browser.new('Safari', '5.1.7'),
-      Browser.new('Firefox', '19.0.2'),
-      Browser.new('Internet Explorer', '9.0'),
-      Browser.new('Chrome', '25.0.1364.160')
-    ]
-
     def check_browser
       user_agent = UserAgent.parse(request.user_agent)
-      unless SupportedBrowsers.detect { |browser| user_agent >= browser }
-        render text: 'Your browser is not supported! Please upgrade your browser to view our website!'
+      if user_agent.browser == 'Internet Explorer' && user_agent.version < '9.0'
+         render text: 'Your browser is not supported! Please upgrade your browser to view our website!'
       end
     end
 end
